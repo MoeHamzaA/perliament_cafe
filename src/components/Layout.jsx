e4a55244e9bec67bf3a293ad55a17d4b3fa82d80
Hamza Ajmal <mhamzaajmal@hotmail.com>
2026-03-29 01:05:06 -0400

    Added features

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,16 +1,38 @@
-import { Outlet } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useLocation, useOutlet } from 'react-router-dom';
+import { AnimatePresence, motion } from 'framer-motion';
 import DemoBanner from './DemoBanner';
 import Navbar from './Navbar';
 import Footer from './Footer';
 
 export default function Layout() {
+  const location = useLocation();
+  // useOutlet gives us the actual React elements for the current page
+  const currentOutlet = useOutlet();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
   return (
     <div className="min-h-screen flex flex-col">
       <DemoBanner />
       <Navbar />
-      <main className="flex-1">
-        <Outlet />
-      </main>
+      
+      <AnimatePresence mode="wait">
+        <motion.main
+          key={location.pathname}
+          initial={{ opacity: 0, y: 15 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -15 }}
+          transition={{ duration: 0.4, ease: "easeOut" }}
+          className="flex-1 flex flex-col"
+        >
+          {/* We clone the outlet and attach the key so Framer Motion "freezes" the exiting page */}
+          {currentOutlet ? React.cloneElement(currentOutlet, { key: location.pathname }) : null}
+        </motion.main>
+      </AnimatePresence>
+      
       <Footer />
     </div>
   );
